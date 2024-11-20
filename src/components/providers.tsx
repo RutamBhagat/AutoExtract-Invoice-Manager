2d9c2d51bb5171d0e384f5f42b4f00d7cca405ca
RutamBhagat <rutambhagat@gmail.com>
2024-11-20 04:29:23 -0800

    feat(query): update react-query to version 5.60.6 and integrate QueryClientProvider in Providers component

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,13 +1,18 @@
 import { Toaster } from "@/components/ui/sonner";
 import ZustandProviders from "./zustand-providers";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient()
 
 export default function Providers({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <ZustandProviders>
-      {children}
-      <Toaster />
-    </ZustandProviders>
+     <QueryClientProvider client={queryClient}>
+      <ZustandProviders>
+        {children}
+        <Toaster />
+      </ZustandProviders>
+     </QueryClientProvider>
   );
 }
