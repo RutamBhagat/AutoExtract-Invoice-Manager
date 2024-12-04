1a04be190a96e1f6c83ef44521c1dc519af5555d
RutamBhagat <rutambhagat@gmail.com>
2024-12-04 04:37:54 -0800

    refactor(purchase-order): enhance email payload processing and validation with detailed error messages

--- src/app/api/purchase-order/route.ts
+++ src/app/api/purchase-order/route.ts
@@ -1,8 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 
 import { consola } from "consola";
-// Define Zod schema for input validation
 import { z } from "zod";
+
+const allowedMimeTypes = [
+  "application/pdf",
+  "image/jpeg",
+  "image/png",
+  "image/jpg",
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  "text/csv",
+];
 
 const emailPayloadPartSchema: z.ZodSchema = z.object({
   partId: z.string(),
@@ -76,8 +84,41 @@ export async function POST(request: NextRequest) {
 
     const validationResult = emailSchema.safeParse(body);
     if (!validationResult.success) {
-      return new NextResponse("Invalid request body", { status: 400 });
+      const errors = validationResult.error.errors
+        .map((e) => e.message)
+        .join(",");
+      return new NextResponse(`Invalid request body: ${errors}`, {
+        status: 400,
+      });
     }
+
+    const email = validationResult.data;
+    let extractedData = "";
+
+    const extractHeaderValue = (name: string) => {
+      const header = email.payload.headers.find((h) => h.name === name);
+      return header ? header.value : "";
+    };
+
+    extractedData += `Subject: ${extractHeaderValue("Subject")}\n`;
+    extractedData += `Snippet: ${email.snippet}\n`;
+    extractedData += `From: ${extractHeaderValue("From")}\n`;
+    extractedData += `Date: ${extractHeaderValue("Date")}\n`;
+    extractedData += `To: ${extractHeaderValue("To")}\n`;
+
+    const processAttachments = (part: any) => {
+      if (allowedMimeTypes.includes(part.mimeType)) {
+        extractedData += `Attachment: ${part.mimeType}, ${part.filename}\n`;
+      }
+
+      if (part.parts) {
+        part.parts.forEach(processAttachments);
+      }
+    };
+
+    email.payload.parts.forEach(processAttachments);
+
+    return NextResponse.json({ extractedData });
   } catch (error: any) {
     consola.error("Error in /api/purchase-orders/route.ts:", error);
     return new NextResponse("Internal Server Error", { status: 500 });
